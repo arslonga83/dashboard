b5b10125c9071a3aa89c78151e782ddc1d32bcbc
arslonga83 <jlwood83@gmail.com>
2023-01-11 20:27:52 -0800

    display author on screen

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
   .then(res => res.json())
   .then(data => {
-    console.log(data.urls)
+    console.log(data)
     document.body.style.backgroundImage =`url(${data.urls.full})`
+    document.querySelector('#img-author').textContent = `${data.user.name}`
   })
