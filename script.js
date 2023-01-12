8254f305a0f8606c896ff1a0a6859f03b899aab3
arslonga83 <jlwood83@gmail.com>
2023-01-12 11:36:00 -0800

    add catch block and default background image. fetch object for crypto section

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     document.body.style.backgroundImage =`url(${data.urls.full})`
     document.querySelector('#img-author').textContent = `By: ${data.user.name}`
   })
+  .catch(err => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1451337516015-6b6e9a44a8a3?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzM1MDQwNTg&ixlib=rb-4.0.3&q=80)`
+    document.querySelector('#img-author').textContent = `By: Andrew Ridley`
+  })
+
+  fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
+    .then(res => {
+      if(!res.ok) {
+        throw Error('Something went wrong.')
+      }
+      return res.json()
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
